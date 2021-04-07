d3798686f4748c93db9d54c17d0924616fcee4c3
wujun369 <1254309578@qq.com>
2021-04-07 19:59:35 +0800

    apiserver

---
+++ goweb/apiserver_demo/demo06/handler/handler.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"goweb/apiserver_demo/demo06/pkg/errno"
+	"net/http"
+)
+
+type Response struct {
+	Code int `json:"code"`
+	Message string `json:"message"`
+	Data interface{} `json:"data"`
+}
+
+func SendResponse(c *gin.Context,err error,data interface{})  {//向客户端发送响应信息
+	code, message := errno.DecodeErr(err)
+
+	r := Response{
+		Code: code,
+		Message: message,
+		Data: data,
+	}
+
+	c.JSON(http.StatusOK,r)
+}
---
+++ goweb/apiserver_demo/demo06/router/router.go
@@ -0,0 +1,40 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"goweb/apiserver_demo/demo06/handler/sd"
+	"goweb/apiserver_demo/demo06/handler/user"
+	"goweb/apiserver_demo/demo06/router/middleware"
+	"net/http"
+)
+
+// Load loads the middlewares, routes, handlers.
+func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
+	// Middlewares.
+	g.Use(gin.Recovery())
+	g.Use(middleware.NoCache)
+	g.Use(middleware.Options)
+	g.Use(middleware.Secure)
+	g.Use(mw...)
+	// 404 Handler.
+	g.NoRoute(func(c *gin.Context) {
+		c.String(http.StatusNotFound, "The incorrect API route.")
+	})
+
+	u := g.Group("/v1/user")
+	{
+		u.POST("", user.Create)
+		u.POST("/:username", user.Create)
+	}
+
+	// The health check handlers
+	svcd := g.Group("/sd")
+	{
+		svcd.GET("/health", sd.HealthCheck)
+		svcd.GET("/disk", sd.DiskCheck)
+		svcd.GET("/cpu", sd.CPUCheck)
+		svcd.GET("/ram", sd.RAMCheck)
+	}
+
+	return g
+}
